921b52c4083f260564fff4d8d1c680fb59720da1
jeyang <ych1273@gmail.com>
2022-03-13 04:48:17 +0900

    회원 서비스 테스트 코드 작성

--- 22.0311_SpringCore_Basic/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ 22.0311_SpringCore_Basic/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,8 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
+    //MemberRepository 추상화에도 의존하고, 구체화에도 의존 중이다.
+    //DIP 위반이다.
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
