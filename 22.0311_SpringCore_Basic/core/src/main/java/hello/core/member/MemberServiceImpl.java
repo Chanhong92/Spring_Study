2fc0758bd6553a782fa515eff8e6ee5399723f1e
jeyang <ych1273@gmail.com>
2022-03-14 01:26:37 +0900

    관심사의 분리:Appconfig로 의존관계 주입(DI)

--- 22.0311_SpringCore_Basic/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ 22.0311_SpringCore_Basic/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,9 +2,11 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    //MemberRepository 추상화에도 의존하고, 구체화에도 의존 중이다.
-    //DIP 위반이다.
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
