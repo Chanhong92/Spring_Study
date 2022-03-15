6064cdf5e34c2a481066c9e4fd148b1fa0820553
jeyang <ych1273@gmail.com>
2022-03-16 04:50:55 +0900

    AppConfig 리팩터링, 각 역할이 드러나도록 수정

--- 22.0311_SpringCore_Basic/core/src/main/java/hello/core/AppConfig.java
+++ 22.0311_SpringCore_Basic/core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,11 +11,21 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
+    // 각 역할이 드러나도록 리팩토링을 진행했다.
+    // 어플리케이션 전체 구성이 어떻게 되어있는지 빠르게 파악할 수 있다.
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
