f1314f82f3b91b121585a53cbe1712d0a908dce5
jeyang <ych1273@gmail.com>
2022-03-25 20:22:23 +0900

    스프링은 어떻게 싱글톤을 유지하는가? (@Configuration)

--- 22.0311_SpringCore_Basic/core/src/main/java/hello/core/AppConfig.java
+++ 22.0311_SpringCore_Basic/core/src/main/java/hello/core/AppConfig.java
@@ -15,23 +15,34 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    //어떻게 불릴것인가?
+    //왜 memberRepository가 한번만 호출된거지?
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+    //call AppConfig.discountPolicy
+
     @Bean // Spring 컨테이너에 등록
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
     public DiscountPolicy discountPolicy() {
+        System.out.println("call AppConfig.discountPolicy");
         return new RateDiscountPolicy();
     }
 }
