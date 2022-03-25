c61610e14818d494afe87d1d98d18761babefe5b
jeyang <ych1273@gmail.com>
2022-03-26 01:38:32 +0900

    필터 (컴포넌트 스캔)

--- 22.0311_SpringCore_Basic/core/src/main/java/hello/core/AutoAppConfig.java
+++ 22.0311_SpringCore_Basic/core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core", //basePackages = "hello.core" 는 default
         //예제를 유지하기 위해 @Configuration 어노테이션이 붙은 클래스는 제외했다 (AppConfig)
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
